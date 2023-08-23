cc778f461f36e2565648850a5edf7a2a63ee8ef9
carsonjames1125 <tarheelkid44@yahoo.com>
2023-08-23 12:46:09 -0400

    added mongo db connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { connect, connection } = require('mongoose');
 
 //connecting to mongodb database
 const connectionStr = 
-process.env.MONGODB_URI || '';
+process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
 
 connect(connectionStr, {
     useNewUrlParsers: true,
